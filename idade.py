4bed71562c564d6b91cd58a95e25532dc6db81db
XxmundanexX <jofelipe2018@gmail.com>
2022-10-31 15:24:44 -0300

    idade

---
+++ idade.py
@@ -0,0 +1,9 @@
+idade = int(input("digite sua idade aqui: "))
+if idade < 12:
+    print("criança")
+elif idade < 18:
+    print("adolescente")
+elif idade < 60:
+    print("adulto")
+else:
+    print("idoso")
