ba3ecbdefdd20bf0958736bb5d017d950fc87554
XxmundanexX <jofelipe2018@gmail.com>
2022-12-07 15:08:03 -0300

    executar aplicativos

---
+++ execute.py
@@ -0,0 +1,12 @@
+
+
+import os
+
+a = int(input("Digite o a letra para abrir o aplicativo: "))
+
+if a == 1:
+    os.system("start WinWord.exe")
+    
+if a == 2:
+    os.system("start Chrome.exe")
+    
